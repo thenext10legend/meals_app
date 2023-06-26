a7e5e198558660ef0f50683956916c8d64105bd5
Shivam JhunJhunwala <shivamjhunjhunwala4dec@gmail.com>
2023-06-26 20:31:10 +0530

    153. Displaying Category Items on a Screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -12,6 +14,7 @@ class CategoriesScreen extends StatelessWidget {
         title: Text("Pick your category"),
       ),
       body: GridView(
+        padding: EdgeInsets.all(24),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 1.5,
@@ -19,30 +22,9 @@ class CategoriesScreen extends StatelessWidget {
           mainAxisSpacing: 20,
         ),
         children: [
-          Text(
-            "1",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "2",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "3",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "4",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "5",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "6",
-            style: TextStyle(color: Colors.white),
-          ),
+          //availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
         ],
       ),
     );
