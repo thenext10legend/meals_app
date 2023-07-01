aa473d0f6b30c6f327202c7e7d2524b9da9cd47c
Shivam JhunJhunwala <shivamjhunjhunwala4dec@gmail.com>
2023-07-02 00:28:36 +0530

    196. Finetuning Explicit Animations

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -83,10 +83,14 @@ class _CategoriesScreenState extends State<CategoriesScreen>
               ),
           ],
         ),
-        builder: (context, child) => Padding(
-              padding: EdgeInsets.only(
-                top: 100 - _animationController.value * 100,
-              ),
+        builder: (context, child) => SlideTransition(
+              position: Tween(
+                begin: Offset(0, 0.3),
+                end: Offset(0, 0),
+              ).animate(CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOut,
+              )),
               child: child,
             ));
   }
